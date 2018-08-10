1211518f937564785ba12b134662b3b4d7ac1492
kugjin <rubiaru00@gmail.com>
2018-08-10 14:42:49 +0900

    2단계

--- app.js
+++ app.js
@@ -60,6 +60,49 @@ var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azu
 var bot = new builder.UniversalBot(connector);
 bot.set('storage', tableStorage);
 
-bot.dialog('/', function (session) {
-    session.send('You said ' + session.message.text);
+
+//------------------------------------------------------------------------------
+// 2일차 15 page  수정 - 대화 다이얼 로그 샘플
+// https://docs.microsoft.com/en-us/azure/bot-service/dotnet/bot-builder-dotnet-activities?view=azure-bot-service-3.0
+// conversationUpdate 참고
+bot.on('conversationUpdate', function (message) {
+    if (message.membersAdded) {
+        message.membersAdded.forEach(function (identity) {
+            if (identity.id === message.address.bot.id) {
+                bot.beginDialog(message.address, '/');
+            }
+        });
+    }
 });
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+// 2일차 16 page  수정 - 대화 다이얼 로그 샘플
+// https://docs.microsoft.com/en-us/azure/bot-service/nodejs/bot-builder-nodejs-dialog-prompt?view=azure-bot-service-3.0
+// Send welcome when conversation with bot is started, by initiating the root dialog
+//bot.dialog('/', function (session) {
+//        session.send('You said ' + session.message.text);
+//});
+//------------------------------------------------------------------------------
+bot.dialog('/', [
+    function (session) {
+        // session.send('You said ' + session.message.text);
+        session.send('안녕하세요. 날씨 알림 챗봇입니다.');        
+        builder.Prompts.text(session, "알고 싶은 지역을 알려주세요.");
+    },
+    function (session, results) {
+        session.userData.location = results.response;
+        session.send(`${session.userData.location} 지역이요? 알겠습니다.`);
+        builder.Prompts.choice(session, "오늘 날씨를 알려드릴까요. 주간 날씨를 알려드릴까요", ["오늘날씨", "주간날씨"], { listStyle: builder.ListStyle.button });        
+    },
+    function (session, results) {
+        session.userData.weatherType = results.response.entity;
+        if (session.userData.weatherType == "오늘날씨") {
+            session.send("오늘 날씨는 O도입니다.");
+        } else if (session.userData.weatherType == "주간날씨") {
+            session.send("주간 날씨는 O요일 O도, O요일 O도, O요일 O도입니다.");
+        } else {
+            session.send("대화를 종료합니다.");    
+            session.endDialog();
+        }
+    },
+]);
