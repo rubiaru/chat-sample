c8185fbd1ee061050d253f7cc2ad327fc06171c2
kugjin <rubiaru00@gmail.com>
2018-08-21 17:00:52 +0900

    버그 수정

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ appInsights.setup(process.env.ApplicationInsightsKey);
 appInsights.start();
 //------------------------------------------------------------------------------
 
+// Setup Restify Server
+var server = restify.createServer();
+
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
     //------------------------------------------------------------------------------
